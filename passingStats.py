1148b11d31147304e09720310c1f6622766926fc
Rasmus Säfvenberg <rasmus.safvenberg@gmail.com>
2022-10-29 16:43:08 +0200

    Scripts for performing PCA and computing passing statistics

---
+++ passingStats.py
@@ -0,0 +1,258 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author: Rasmus Säfvenberg
+
+import pandas as pd
+from pandera.typing import DataFrame
+from typing import Tuple
+
+
+def compute_passing_statistics(merged_wide_events: DataFrame) -> DataFrame:
+    """
+    Compute a set of pre-defined passing statistics.
+
+    Parameters
+    ----------
+    merged_wide_events : DataFrame
+        Data frame of all events in wide format.
+
+    Returns
+    -------
+    passing_stats : DataFrame
+        Data frame with all passing statistics per game and player.
+
+    """
+
+    # Include final third/penalty box entries? Also lost balls + offsides
+    passes = merged_wide_events.loc[(
+        merged_wide_events.action.isin(["Pass", "Right corner", "Left corner"]) &
+        ~merged_wide_events.action_list.str.startswith("Goal")
+        )].copy()
+    
+    # Compute statistics
+    final_third_entries = merged_wide_events.loc[
+        merged_wide_events.action.eq("Final third entry")].groupby(
+        ["match_id", "player", "team"], as_index=False).size().rename(
+        columns={"size": "Final third entries"})
+        
+    # Compute number of crosses per player, length and result
+    crosses = passes.loc[passes.action_list.eq("Cross")].groupby(
+        ["match_id", "player", "team", "pass_length", "result_text"], as_index=False).size(
+            ).rename(columns={"size": "Crosses"})
+   
+    # Compute number of wing switching passes per player, length and result
+    wing_switch = passes.loc[passes.wing_switch].groupby(
+        ["match_id", "player", "team", "pass_length", "wing_switch", "result_text"], as_index=False).size(
+            ).rename(columns={"size": "Wing switches"})
+    
+    # Compute number of key passes per player, length and result
+    key_passes = passes.loc[passes.key_pass].groupby(
+        ["match_id", "player", "team", "pass_length", "key_pass", "result_text"], as_index=False).size(
+            ).rename(columns={"size": "Key passes"})
+            
+    # Compute number of progressive passes per player, length and result
+    passes_into_box = passes.loc[passes.pass_into_box].groupby(
+        ["match_id", "player", "team", "pass_length", "pass_into_box", "result_text"], as_index=False).size(
+            ).rename(columns={"size": "Passes into the box"})
+            
+    # Compute number of assists per player, length and result
+    assists = passes.loc[passes.assist].groupby(
+        ["match_id", "player", "team", "pass_length", "assist", "result_text"], as_index=False).size(
+            ).rename(columns={"size": "Assists"})
+    
+    # Compute number of progressive passes per player, length and result
+    progressive_passes = passes.loc[passes.progressive_pass].groupby(
+        ["match_id", "player", "team", "pass_length", "progressive_pass", "result_text"], as_index=False).size(
+            ).rename(columns={"size": "Progressive passes"})
+ 
+    # Compute total passes per player, length and result
+    total_passes = passes.groupby(
+        ["match_id", "player", "team", "pass_length", "result_text"], as_index=False).size(
+            ).rename(columns={"size": "Passes"})
+    
+    # Combine all passing types into one data frame
+    passing_stats = (
+        wing_switch.drop("wing_switch", axis=1).merge(
+        key_passes.drop("key_pass", axis=1), how="outer").merge(
+        passes_into_box.drop("pass_into_box", axis=1), how="outer").merge(
+        crosses, how="outer").merge(
+        assists.drop("assist", axis=1), how="outer").merge(
+        progressive_passes.drop("progressive_pass", axis=1), how="outer").merge(
+        total_passes, how="outer")
+            ).sort_values(["player", "team"]).reset_index(drop=True)
+
+    return passing_stats
+
+
+def compute_possession_adjusted_stats(merged_wide_events: DataFrame, 
+                                      passing_stats: DataFrame) -> Tuple[DataFrame,
+                                                                         DataFrame]:
+    """
+    Compute possession adjusted stats for passes and corners for each game 
+    and player.
+
+    Parameters
+    ----------
+    merged_wide_events : DataFrame
+        Data frame of all events in wide format.
+    passing_stats : DataFrame
+        Data frame with all passing statistics per game and player.
+
+    Returns
+    -------
+    passing_stats_possession_adjusted : DataFrame 
+        Data frame with all passing statistics per game and player, adjusted 
+        by the team possession in the given game.
+    corners_possession_adjusted : DataFrame
+        Data frame with statistics of corners by game and player, adjusted
+        by the team possession in the given game.
+
+    """
+    # Get all passes 
+    passes = merged_wide_events.loc[merged_wide_events.action.isin(["Pass", 
+                                                                    "Left corner",
+                                                                    "Right corner"])]
+        
+    # Compute number of corners per player and result
+    corners = passes.loc[passes.action_list.str.contains("corner")].groupby(
+        ["match_id", "player", "team", "result_text"], as_index=False).size(
+            ).rename(columns={"size": "Corners"})
+            
+    # Compute the number of passes per game and team
+    game_team_passes = passes.groupby(["match_id", "team"], as_index=False).size().rename(
+        columns={"size": "team_passes"})
+    
+    # Compute the number of passes per game
+    game_passes = passes.groupby(["match_id"], as_index=False).size().rename(
+        columns={"size": "game_passes"})
+    
+    # Add the total number of passes to the team game pass data frame
+    game_team_passes = game_team_passes.merge(game_passes, on="match_id")
+    
+    # Compute possession as the ratio of team passes and total passes
+    game_team_passes["possession"] = game_team_passes.apply(
+        lambda x: x.team_passes / x.game_passes, axis=1)
+    
+    # Combine passing statistics with possession
+    passing_stats_possession = passing_stats.merge(game_team_passes.drop(["team_passes",
+                                                                          "game_passes"],
+                                                                         axis=1),
+                                                   on=["match_id", "team"],
+                                                   how="inner")
+        
+    # Create a copy of the data to compute possession adjusted stats
+    passing_stats_possession_adjusted = passing_stats_possession.copy()
+    
+    # Compute possession adjusted passing statistics
+    for column in passing_stats_possession.columns[5:-1]:
+         passing_stats_possession_adjusted[column] = passing_stats_possession.apply(
+            lambda x: x[column] / x.possession, axis=1)
+          
+    # Combine corner statistics with possession
+    corners_possession_adjusted = corners.merge(
+        game_team_passes.drop(["team_passes", "game_passes"], axis=1),
+        on=["match_id", "team"], how="inner")
+    
+    # Compute possession adjusted corner statistics
+    corners_possession_adjusted["Corners"] = corners_possession_adjusted.apply(
+            lambda x: x["Corners"] / x.possession, axis=1)
+         
+    # Compute possession adjusted corner statistics over the entire season
+    corners_possession_adjusted = corners_possession_adjusted.groupby(
+        ["player", "team", "result_text"], 
+           as_index=False).sum().drop(["possession", "match_id"], axis=1)
+    
+    # Compute possession adjusted passing statistics over the entire season
+    passing_stats_possession_adjusted = passing_stats_possession_adjusted.groupby(
+        ["player", "team", "pass_length", "result_text"], as_index=False).sum().drop(
+            ["possession", "match_id"], axis=1)
+    
+    return passing_stats_possession_adjusted, corners_possession_adjusted
+       
+
+def combine_possession_adjusted_and_minutes(passing_stats_possession_adjusted: DataFrame,
+                                            corners_possession_adjusted: DataFrame,
+                                            minutes_played_season: DataFrame) -> DataFrame:
+    """
+    Combine possession adjusted passing statistics with minutes played to 
+    compute statistics per 90 minutes.
+
+    Parameters
+    ----------
+    passing_stats_possession_adjusted : DataFrame
+        Data frame with all passing statistics per game and player, adjusted 
+        by the team possession in the given game.
+    corners_possession_adjusted : DataFrame
+        Data frame with statistics of corners by game and player, adjusted
+        by the team possession in the given game.
+    minutes_played_season : DataFrame
+        Data frame with the total amount of minutes played during the season 
+        for all players.
+
+    Returns
+    -------
+    all_passing_adjusted_per_90 : DataFrame
+        All passing events, standardized by minutes played, and possession adjusted
+        per 90 minutes.
+
+    """
+
+    # Combine possession adjusted passing statistics with minutes played
+    passing_possession_minutes = passing_stats_possession_adjusted.merge(
+        minutes_played_season, left_on=["player", "team"], right_on=["player", "team"])
+    
+    # Passing variables to consider for standardization
+    variables = ["Wing switches", "Key passes", "Passes into the box",
+                 "Crosses", "Assists", "Progressive passes", "Passes"]
+    
+    # Compute possession adjusted passing statistics per 90'
+    passing_per_90 = [passing_possession_minutes[[column, "minutes"]].apply(
+        lambda x: 90 * x / x.minutes, axis=1).drop("minutes", axis=1) for 
+        column in variables]  
+    
+    # Add the standardized metrics and combine with player name and team
+    passing_per_90 = pd.concat([passing_possession_minutes.drop(variables, axis=1), 
+                                         pd.concat(passing_per_90, axis=1)], axis=1)
+    
+    # Combine possession adjusted corners with minutes played
+    corners_per_90 = corners_possession_adjusted.merge(
+        minutes_played_season, left_on=["player", "team"], right_on=["player", "team"])
+    
+    # Convert the possession adjusted number of corners to be per 90'
+    corners_per_90["Corners"] = corners_per_90.apply(
+        lambda x: 90 * x.Corners / x.minutes, axis=1)
+    
+    # Convert corners from long to wide
+    corners_adjusted_per_90 = corners_per_90.drop("minutes", axis=1).pivot(
+        index=["player", "team"], columns=["result_text"]).fillna(0)
+    
+    # Convert passing from long to wide
+    passing_adjusted_per_90 = passing_per_90.drop("minutes", axis=1).pivot(
+        index=["player", "team"], columns=["pass_length", "result_text"])
+    
+    # Rename columns by stacking hierarchical indexes
+    passing_adjusted_per_90.columns = ['_'.join(col).strip() for col in
+                                       passing_adjusted_per_90.columns.values]
+    corners_adjusted_per_90.columns = ['_'.join(col).strip() for col in 
+                                       corners_adjusted_per_90.columns.values]
+
+    # Reset the index to create common columns for merging merging
+    passing_adjusted_per_90.reset_index(inplace=True)
+    corners_adjusted_per_90.reset_index(inplace=True)
+
+    # Combine per 90' possession adjusted passing with corners
+    all_passing_adjusted_per_90 = passing_adjusted_per_90.merge(
+        corners_adjusted_per_90, how="outer").fillna(0)
+
+    # Change the index back to the previous version prior to merging
+    all_passing_adjusted_per_90.set_index(["player", "team"], inplace=True)
+
+    # Compute the column sums 
+    col_sums = all_passing_adjusted_per_90.sum(axis=0)
+    
+    # Drop all columns that only have 0 as a value
+    all_passing_adjusted_per_90.drop(col_sums[col_sums.eq(0)].index, axis=1, inplace=True)
+    
+    return all_passing_adjusted_per_90
+
+
---
+++ pca.py
@@ -0,0 +1,305 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author: Rasmus Säfvenberg
+
+import sys
+import numpy as np
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.metrics import silhouette_score
+from sklearn_extra.cluster import KMedoids
+from tqdm import tqdm
+import seaborn as sns
+import matplotlib.pyplot as plt   
+from typing import Tuple
+from pandera.typing import DataFrame
+
+
+def visualize_pca(all_passing_adjusted_per_90_scaled: DataFrame,
+                  all_passing_adjusted_per_90: DataFrame) -> None:
+    """
+    Visualize the explained variance obtained from PCA.
+
+    Parameters
+    ----------
+    all_passing_adjusted_per_90_scaled : DataFrame
+        All passing events, standardized by minutes played, possession adjusted 
+        and standardized per 90 minutes.
+    all_passing_adjusted_per_90 : DataFrame
+        All passing events, standardized by minutes played, and possession adjusted
+        per 90 minutes.
+
+    Returns
+    -------
+    None. Instead a plot is created.
+
+    """
+    
+    # Create a PCA object for selecting the components
+    pca = PCA().fit(all_passing_adjusted_per_90_scaled)
+
+    # Find the number of columns, i.e., features    
+    nr_columns = len(all_passing_adjusted_per_90.columns)
+    
+    # Initialize figure
+    plt.figure(figsize=(12,8))
+    
+    # Create a data frame for plotting
+    explained_variance = pd.DataFrame(
+        data=np.around(pca.explained_variance_ratio_, 3),
+        index=[f'PC{i}' for i in range(1, nr_columns+1)],
+        columns=["ExplainedVariance"]).reset_index().rename(columns={"index": "PC"})
+    
+    # Compute the cumulative explained variance
+    explained_variance["CumulativeExplained"] = np.cumsum(explained_variance["ExplainedVariance"])
+    
+    # Create a barplot for the explained variance for each component
+    ax = sns.barplot(x="PC", y="ExplainedVariance",
+                     data=explained_variance, 
+                     color="#E8112d")
+    
+    # Create a scatterplot between the components and the cumulative explained variance
+    sns.scatterplot(x="PC", y="CumulativeExplained",
+                    data=explained_variance, alpha=0.7,
+                    color="#006AB3")
+    
+    # Create a lineplot for the cumulative explained variance of the components
+    sns.lineplot(x="PC", y="CumulativeExplained",
+                 data=explained_variance, alpha=0.7,
+                 color="#006AB3")
+    
+    # Remove whitespace between bars and plot axes
+    ax.margins(x=0)
+    
+    # Specify axis ticks on the y axis
+    ax.set_yticks(ticks=np.linspace(0,1,11))
+    
+    # Specfiy axis text size
+    ax.tick_params(axis="x", rotation=45, labelsize=10)
+    ax.tick_params(axis="y", labelsize=12)
+
+    # Specify axis labels
+    ax.set_xlabel("Principal component", fontsize=14)
+    ax.set_ylabel("Proportion of variance explained", fontsize=14)
+    
+    # Specify the boundaries of the y axis
+    ax.set_ylim(0, 1)
+
+
+def visualize_pca_loadings(all_passing_adjusted_per_90_scaled: DataFrame, 
+                           all_passing_adjusted_per_90: DataFrame) -> None:
+    """
+    Visualize the loadings of each variable on the principal components
+
+    Parameters
+    ----------
+    all_passing_adjusted_per_90_scaled : DataFrame
+        All passing events, standardized by minutes played, possession adjusted 
+        and standardized per 90 minutes.
+    all_passing_adjusted_per_90 : DataFrame
+        All passing events, standardized by minutes played, and possession adjusted 
+        per 90 minutes.
+
+    Returns
+    -------
+    None. Instead a plot is created.
+
+    """
+    
+    # Create a new PCA object with the desired amount of components
+    pca = PCA().fit(all_passing_adjusted_per_90_scaled)
+    
+    # Create a data frame containing the PC loadings
+    loadings = pd.DataFrame(
+       data=pca.components_.T * np.sqrt(pca.explained_variance_), 
+       columns=[f'PC{i}' for i in range(1, len(all_passing_adjusted_per_90.columns)+1)],
+       index=all_passing_adjusted_per_90.columns
+     )   
+
+    # Initialize figure
+    plt.figure(figsize=(12, 8))    
+    
+    # Plot the heatmap of loadings    
+    ax = sns.heatmap(loadings, cmap="RdYlGn")
+
+    # Specfiy axis text size
+    ax.tick_params(axis="both", labelsize=12)
+
+    # Get the colorbar
+    cbar = ax.collections[0].colorbar
+    
+    # Specify font size for the colorbar
+    cbar.ax.tick_params(labelsize=16)
+    
+
+def visualize_number_of_clusters(pca_data: DataFrame) -> None:
+    """
+    Visualize the relationship between the number of clusters and cluster 
+    tightness via Silhouette score. A higher score indicates more distinct clusters.
+
+    Parameters
+    ----------
+    pca_data : DataFrame
+        The data projected onto the PCA bases.
+
+    Returns
+    -------
+    None. Instead a plot is created.
+
+    """
+    # Initialize an empty array for storing silhouette scores
+    silhouette = np.zeros((19, 2))
+    
+    # Examine k = 2, ..., 20
+    for k in range(2, 21):
+        # Compute PAM on the PCA data
+        k_medoids = KMedoids(n_clusters=k, random_state=0).fit(pca_data)
+        
+        # Save the current value of k in the first column
+        silhouette[k-2, 0] = k
+        
+        # Compute the silhouette score for the current clustering
+        silhouette[k-2, 1] = silhouette_score(pca_data, k_medoids.labels_)
+        
+    # Find the largest silhouette scores    
+    best_k = silhouette[:, 1].argsort()[::-1] + 2
+    
+    # Create a data farme for plotting
+    plot_data = pd.DataFrame(silhouette, columns=["k", "Silhouette"])
+
+    # Initialize figure    
+    plt.figure(figsize=(12, 8))    
+
+    # Create a barplot showing the best values for k
+    ax = sns.barplot(data=plot_data, x="k", y="Silhouette", order=best_k, 
+                     color="#E8112d")
+    
+    # Specify axis labels
+    ax.set_xlabel("Number of clusters", fontsize=14)
+    ax.set_ylabel("Silhouette score", fontsize=14)
+    
+    
+def parallel_analysis(input_data: np.array, iterations: int=100, 
+                      centile: int=99, plot: bool=True) -> Tuple[int, np.array]:
+    """
+    Perform parallel analysis based on Horn (1965) and the improvements 
+    suggested by Glorfeld (1995) to select the number of components in PCA.
+
+    Parameters
+    ----------
+    input_data : np.array
+        The data on which the parallel analysis is based.
+    iterations : int, optional
+        The number of iterations to run for sampling. The default is 100.
+    centile : int, optional
+        Computes a Monte Carlo estimate for estimating bias. Accepts values 
+        between 0 and 99, where 0 corresponds to using the mean.
+        The default is 99.
+    plot : bool, optional
+        If a plot should be created. The default is True.
+
+    Returns
+    -------
+    retained : int
+        The number of retained components.
+    adj_eigenvalues : np.array
+        Array of all adjusted eigenvalues.
+
+    References
+    ----------
+    Horn, J. L. (1965). A rationale and test for the number of factors in factor
+    analysis. Psychometrika, 30(2), 179–185.
+    
+    Glorfeld, L. W. (1995). An improvement on Horn's parallel analysis methodology 
+    for selecting the correct number of factors to retain.
+    Educational and Psychological Measurement, 55(3), 377–393. 
+
+    """
+    # Set seed for reproducible results
+    np.random.seed(0)
+    
+    # Get the number of observations (n) and variables (p)
+    n, p = input_data.shape
+    
+    # Compute the correlation matrix of variables from the data
+    corr_matrix = np.corrcoef(input_data, rowvar=False)
+    
+    # Compute the eigenvalues
+    eigenvalues, _ = np.linalg.eig(corr_matrix)    
+
+    # Array to store simulated eigenvalues
+    simulated_eigenvalues = np.zeros((iterations, p))
+
+    # Loop over the set of iterations
+    for k in tqdm(range(iterations)):
+        # Sample random a set of (n x p) observations from a standard normal distribution 
+        rnd_normal = np.random.normal(size=(n, p))
+
+        # Compute the correlation matrix of variables from the data
+        corr_rnd = np.corrcoef(rnd_normal, rowvar=False)
+        
+        # Compute the eigenvalues
+        eigenvalues_rnd, _ = np.linalg.eig(corr_rnd)
+        
+        # Save the eigenvalues obtained from the random sampling
+        simulated_eigenvalues[k, ] = eigenvalues_rnd
+
+    # If the centile method should be used        
+    if centile > 0 and centile < 100:
+        rnd_ev = np.quantile(simulated_eigenvalues, centile/100, axis=0)
+    elif centile == 0:
+        rnd_ev = np.mean(simulated_eigenvalues, axis=0)
+    else: 
+        sys.exit("Centile need to be between 0 and 99.")
+
+    # Compute bias for the random eigenvalues
+    bias = rnd_ev - 1
+    
+    # Compute adjusted eigenvalues based on the bias
+    adj_eigenvalues = eigenvalues - bias
+
+    # Count the number of components to retain
+    retained = (adj_eigenvalues > 1).sum()
+
+    # To plot or not to plot
+    if plot:
+        # Initialize figure
+        fig, ax = plt.subplots(figsize=(12, 8))       
+        
+        # Plot the unadjusted eigenvalues
+        plt.plot(eigenvalues, "-o", color="tab:blue", label="Unadjusted eigenvalues")
+           
+        # Plot the adjusted eigenvalues to retain
+        plt.plot(adj_eigenvalues[adj_eigenvalues > 1], "-o", color="black", 
+                 label="Adjusted eigenvalues (retained)")
+        
+        # Plot the adjusted eigenvalues to not retain
+        plt.plot(adj_eigenvalues, "-o", color="black", 
+                 label="Adjusted eigenvalues (unretained)", markerfacecolor="none")
+        
+        # Plot the random eigenvalues
+        plt.plot(rnd_ev, "-o", color="tab:red", label="Random eigenvalues")
+        
+        # Add a line indicating the decision boundary
+        plt.axhline(y=1, color="lightgray", alpha=0.5)
+        
+        # Add legend
+        plt.legend()
+        
+        # Specify axis labels
+        ax.set_xlabel("Number of components", fontsize=14)
+        ax.set_ylabel("Eigenvalue", fontsize=14)
+        ax.set_title("Parallel Analysis")
+    
+        # Specify the axis ticks
+        ticks = [i for i in range(0, eigenvalues.shape[0]+1, 5)]
+        ax.xaxis.set_ticks(ticks)
+        
+        # Set the axis tick labels to be 1 higher
+        ax.set_xticklabels([i + 1 for i in ticks])
+        
+        # Show plot
+        plt.show()  
+
+    return retained, adj_eigenvalues
+
