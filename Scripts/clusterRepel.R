538793a03f2d4e25d6e95a568213e2e45955461a
Rasmus Säfvenberg <rasmus.safvenberg@gmail.com>
2022-11-29 21:13:26 +0100

    Reverting to old font

--- Scripts/clusterRepel.R
+++ Scripts/clusterRepel.R
@@ -1,17 +1,11 @@
 library(ggplot2)
 library(ggrepel)
 library(stringr)
-library(extrafont)
-library(showtext)
 
 # Specify the working directory
 if(length(str_split(getwd(), "/Scripts")[[1]]) != 2){
   setwd("Scripts")
 }
-
-# Import extra font
-font_add("Roboto", "../Fonts/Roboto-Black.ttf")
-showtext_auto()
 
 # Read the data
 plot_data <- read.csv("../Data/plot_data.csv")
@@ -28,16 +22,16 @@ ggplot(plot_data, aes(PC1, PC2,
                                               "5: The unwilling passer", "6: The winger", 
                                               "7: The defensive passer", "8: The advanced playmaker")), 
                      label = player)) +
-  geom_point() + geom_text_repel(show.legend = FALSE, max.overlaps = 8, size=10) + 
+  geom_point() + geom_text_repel(show.legend = FALSE, max.overlaps = 6, size = 4) + 
   scale_color_manual(name = "", values = cbPalette) +
   theme_classic() + theme(legend.position = "top", panel.grid = element_blank(),
                           axis.line.x.bottom = element_line(color="#C0C0C0"),
                           axis.line.y.left =  element_line(color="#C0C0C0"),
-                          axis.text = element_text(size=30, color="black"), 
-                          axis.title = element_text(size=30),
-                          legend.text = element_text(size=30)) + 
+                          axis.text = element_text(size=12, color="black"), 
+                          axis.title = element_text(size=12),
+                          legend.text = element_text(size=12)) + 
   guides(label = "none")
 
 # Save the figure
-ggsave("../Figures/cluster_names_repel.png", width = 10, height = 6)
+ggsave("../Figures/cluster_names_repel.png", width = 12, height = 8)
 
